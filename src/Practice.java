374520817be2a7d77ade64a6b8fc214d3d42c85b
KJ <kjmiclea@gmail.com>
2026-04-16 16:30:54 -0700

    Bug fix for maxDiff.

--- src/Practice.java
+++ src/Practice.java
@@ -8,8 +8,8 @@ public class Practice {
      * @return the difference between the largest and smallest number
      */
     public static int maxDiff(int[] nums) {
-        int max = 0;
-        int min = 0;
+        int max = nums[0];
+        int min = nums[0];
         for (int num : nums) {
             if (num > max) { max = num; }
             if (num < min) { min = num; } 
